94d38f9648f879729089898abea4c45f224e83d8
Abhishek Chaurasia <abhi24061997@gmail.com>
2020-09-08 11:25:24 +0530

    Added README

---
+++ README.md
@@ -0,0 +1,25 @@
+<!-- ABOUT THE PROJECT -->
+## Sudoku Solver
+
+[![Product Name Screen Shot][product-screenshot]](https://custom-sudoku-solver)
+
+Sudoku Solver is a 9x9 Standard Sudoku Game. It's custom solver can also any given valid sudoku.
+
+Features:
+* In the Sudoku Solver, user can fill their own unsolved puzzle and get the solved puzzle.
+* In between game, user can check if it is a valid move or not.
+* In case, user gets stuck and is unable to solve. User can see the answer by clicking on solve.  
+
+### Built With
+
+* [Django](https://www.djangoproject.com/)
+* [Bootstrap](https://getbootstrap.com)
+* [JQuery](https://jquery.com)
+* [HTML](https://en.wikipedia.org/wiki/HTML)
+
+<!-- Author -->
+## Author
+[iamabhishek0](https://github.com/iamabhishek0)
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[product-screenshot]: templates/images/sudoku-solver.png
--- sudokusolver/views.py
+++ sudokusolver/views.py
@@ -19,23 +19,21 @@ def custom(request):
     if(request.method=="POST"):
         body_unicode = request.body.decode('utf-8')
         json_data = json.loads(body_unicode)
-        grid = []
-        row = []
-        cnt=1
+        grid, row = [], []
+        cnt = 1
         for x in json_data:
             row.append(x['value'])
             cnt = cnt + 1
             if cnt == 10:
                 grid.append(row)
                 row = []
-                cnt=1
-        print(grid)
+                cnt = 1
+
         if isValidConfig(grid):
             sudoku_grid = copy.deepcopy(SudokuGenerator(grid).grid)
             response_data = {}
             response_data['sudoku_grid'] = sudoku_grid
             response_data['valid'] = True
-            # print(json.dumps(sudoku_grid))
             return JsonResponse(data=response_data, status=200)
 
         else:
